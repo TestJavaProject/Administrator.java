f3f6eb9a91712a81b18477be04dc83699e956854
Reuben Frank <reuben.a.frank@gmail.com>
2016-02-19 23:52:34 -0600

    -finished Product.java and updated Order to contain an ArrayList of Products

--- Order.java
+++ Order.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 public class Order{
 	private Bill b;
 	private int orderId;
 	private Date creationDate;
 	private String dateShipped, status;
-	
+	private ArrayList<Product> products;
 }
---
+++ Product.java
@@ -0,0 +1,6 @@
+public class Product{
+
+	private int productId;
+	private String category, description;
+	private long price;
+}
