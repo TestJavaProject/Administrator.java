48efb2373aab756671d247641bd7094555554292
Reuben Frank <reuben.a.frank@gmail.com>
2016-02-19 23:35:53 -0600

    -updated Administrator.java and created User.java

--- Administrator.java
+++ Administrator.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Administrator{
+public class Administrator extends User{
 	private ArrayList<String> permissions;
 	private ArrayList<String> role;
 	private User us;
---
+++ User.java
@@ -0,0 +1,13 @@
+public class User extends Administrator{
+
+	private String name, email, userName, password, loginStatus;
+	private Customer c;
+	
+	public User(Administrator a, String nm, String em, String un, String pass, String ls){
+		this.a = a; name = nm; email = em; userName = un; password = pass; loginStatus = ls;
+	}
+	
+	public boolean verifyLogin(String us, String pass){
+		return (us.equals(userName) && pass.equals(password));
+	}
+}
